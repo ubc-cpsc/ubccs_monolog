480bc3988b1ad853ac691795cc3621c2a7a624cc
Ramil Gonzaga <rgonzaga@cs.ubc.ca>
2024-04-23 16:43:55 -0700

    Add CLI condition check to DrupalMailHandler
    This commit introduces a CLI condition check to the DrupalMailHandler class, preventing emails from being sent during CLI requests.

--- src/Logger/Handler/DrupalMailHandler.php
+++ src/Logger/Handler/DrupalMailHandler.php
@@ -3,7 +3,9 @@
 namespace Drupal\ubccs_monolog\Logger\Handler;
 
 use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\monolog\Logger\ConditionResolver\ConditionResolverInterface;
 use Drupal\monolog\Logger\Handler\DrupalMailHandler as MonologDrupalMailHandler;
+use Monolog\LogRecord;
 
 /**
  * DrupalMailHandler uses the Drupal's core mail manager to send Log emails.
@@ -16,12 +18,15 @@ class DrupalMailHandler extends MonologDrupalMailHandler {
    * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
    * @param int|string|\Monolog\Level $level
    *   The minimum logging level at which this handler will be triggered.
+   * @param \Drupal\monolog\Logger\ConditionResolver\ConditionResolverInterface $conditionResolver
+   *   The condition resolver.
    * @param bool $bubble
    *   The bubbling behavior.
    */
   public function __construct(
     ConfigFactoryInterface $config_factory,
     $level,
+    private readonly ConditionResolverInterface $conditionResolver,
     bool $bubble = TRUE
   ) {
     $to = $config_factory->get('ubccs_monolog.settings')->get('notification_email') ??
@@ -29,6 +34,16 @@ class DrupalMailHandler extends MonologDrupalMailHandler {
 
     parent::__construct($to, $level, $bubble);
 
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function write(LogRecord $record): void {
+    // Do not send emails when it is a CLI request.
+    if (!$this->conditionResolver->resolve()) {
+      $this->send((string) $record->formatted, [$record]);
+    }
   }
 
 }
